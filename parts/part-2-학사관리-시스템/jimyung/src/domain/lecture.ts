2fb89949572601cd78630529889be71900fdf6f7
jimyungkoh <kojimyung1206@naver.com>
2025-01-26 19:54:11 +0900

    feat: 강의 도메인 생성
    - 강의 타입(전공필수/선택, 교양필수/선택)
    - 강의 정원 증가/감소
    - 강의 속성(학점 및 정원 값) 유효성 검증

---
+++ parts/part-2-학사관리-시스템/jimyung/src/domain/lecture.ts
@@ -0,0 +1,79 @@
+import { nanoid } from 'nanoid';
+import { Values } from '../type';
+import { ErrorCodes, InvalidException } from './exception';
+import { TimeSlot } from './time-slot';
+
+export const CourseType = {
+  Required: {
+    Major: '전공필수',
+    Ge: '교양필수',
+  },
+  Elective: {
+    Major: '전공선택',
+    Ge: '교양선택',
+  },
+} as const;
+
+export type CourseType = Values<{
+  [K in keyof typeof CourseType]: Values<(typeof CourseType)[K]>;
+}>;
+
+export type LectureProps = {
+  readonly id: string;
+  readonly name: string;
+  readonly credits: number;
+  readonly professorId: string;
+  readonly courseType: CourseType;
+  readonly capacity: number;
+  readonly timeSlots: TimeSlot[];
+};
+
+export class Lecture {
+  readonly id: string;
+  readonly name: string;
+  readonly credits: number;
+  readonly professorId: string;
+  readonly courseType: CourseType;
+  readonly timeSlots: TimeSlot[];
+  #capacity: number;
+
+  constructor(props: Omit<LectureProps, 'id' | 'timeSlots'>) {
+    this.#validateProps(props);
+
+    this.id = nanoid();
+    this.timeSlots = [];
+    this.name = props.name;
+    this.credits = props.credits;
+    this.professorId = props.professorId;
+    this.courseType = props.courseType;
+    this.#capacity = props.capacity;
+  }
+
+  decreaseCapacity(): this {
+    if (this.#capacity === 0) {
+      throw new InvalidException(ErrorCodes.Lecture.INVALID_CAPACITY);
+    }
+
+    this.#capacity--;
+
+    return this;
+  }
+
+  increaseCapacity(): this {
+    this.#capacity++;
+
+    return this;
+  }
+
+  get capacity() {
+    return this.#capacity;
+  }
+
+  #validateProps(props: Omit<LectureProps, 'id' | 'timeSlots'>) {
+    if (props.credits < 1) {
+      throw new InvalidException(ErrorCodes.Lecture.INVALID_CREDITS);
+    } else if (props.capacity < 0) {
+      throw new InvalidException(ErrorCodes.Lecture.INVALID_CAPACITY);
+    }
+  }
+}
