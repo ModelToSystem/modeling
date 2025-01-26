95e1f4ce4256632f99c614eaf57337db646e691a
jimyungkoh <kojimyung1206@naver.com>
2025-01-26 20:02:04 +0900

    test: 학생 도메인 테스트 케이스 작성
    - 수강신청
      - 수강 가능 학점, 강의 정원 변화 검증
      - 학점 부족 시 예외 처리
    - 수강 취소
      - 학점과 정원 복구 검증

---
+++ parts/part-2-학사관리-시스템/jimyung/test/domain/user/unit/student.spec.ts
@@ -0,0 +1,83 @@
+import {
+  CourseType,
+  ErrorCodes,
+  InvalidException,
+  Lecture,
+  Student,
+} from 'parts/part-2-학사관리-시스템/jimyung/src/domain';
+
+describe('Student', () => {
+  describe('수강 신청', () => {
+    it('수강 신청 시 수강 가능 학점 감소 및 강의 수강 가능 인원 감소', () => {
+      // given
+      const student = new Student({ id: '1', allowedCredits: 10 });
+      const lecture = new Lecture({
+        name: 'Math',
+        credits: 3,
+        professorId: '1',
+        courseType: CourseType.Required.Ge,
+        capacity: 10,
+      });
+      const initialCapacity = lecture.capacity;
+      const initialAllowedCredits = student.allowedCredits;
+
+      // when
+      student.enroll(lecture);
+
+      // then
+      expect(lecture.capacity).toBe(initialCapacity - 1);
+      expect(student.allowedCredits).toBe(
+        initialAllowedCredits - lecture.credits,
+      );
+    });
+
+    it('수강 신청 시 수강 가능 학점이 부족할 경우 예외 발생', () => {
+      // given
+      const student = new Student({ id: '1', allowedCredits: 10 });
+      const lecture = new Lecture({
+        name: 'Math',
+        credits: 11,
+        professorId: '1',
+        courseType: CourseType.Required.Ge,
+        capacity: 10,
+      });
+      const initialCapacity = lecture.capacity;
+      const initialAllowedCredits = student.allowedCredits;
+
+      // when
+      const creditOverflowedEnrollment = () => student.enroll(lecture);
+
+      // then
+      expect(creditOverflowedEnrollment).toThrow(
+        new InvalidException(ErrorCodes.Student.INSUFFICIENT_CREDITS),
+      );
+      expect(lecture.capacity).toBe(initialCapacity);
+      expect(student.allowedCredits).toBe(initialAllowedCredits);
+    });
+  });
+
+  describe('수강 신청 취소', () => {
+    it('수강 신청 취소 시 수강 가능 학점 증가 및 강의 수강 가능 인원 증가', () => {
+      // given
+      const student = new Student({ id: '1', allowedCredits: 10 });
+
+      const lecture = new Lecture({
+        name: 'Math',
+        credits: 3,
+        professorId: '1',
+        courseType: CourseType.Required.Ge,
+        capacity: 10,
+      });
+      const initialCapacity = lecture.capacity;
+      const initialAllowedCredits = student.allowedCredits;
+
+      // when
+      student.enroll(lecture);
+      student.cancelEnrollment(lecture);
+
+      // then
+      expect(lecture.capacity).toBe(initialCapacity);
+      expect(student.allowedCredits).toBe(initialAllowedCredits);
+    });
+  });
+});
