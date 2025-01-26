6dbc934a9ce228e641d93a92e3100e10c85982a0
jimyungkoh <kojimyung1206@naver.com>
2025-01-26 20:00:07 +0900

    feat: 학생 도메인 생성
    - 수강신청/취소 관련 메서드
    - 수강 가능 학점 관리

---
+++ parts/part-2-학사관리-시스템/jimyung/src/domain/user/student.ts
@@ -0,0 +1,39 @@
+import { User } from '.';
+import { Enrollment } from '../enrollment';
+import { ErrorCodes, InvalidException } from '../exception';
+import { Lecture } from '../lecture';
+
+export class Student implements User {
+  readonly id: string;
+  #allowedCredits: number;
+  #enrollments: Map<string, Enrollment>;
+
+  constructor(props: User & { allowedCredits: number }) {
+    this.id = props.id;
+    this.#allowedCredits = props.allowedCredits;
+    this.#enrollments = new Map();
+  }
+
+  enroll(lecture: Lecture): Enrollment {
+    if (this.#allowedCredits < lecture.credits) {
+      throw new InvalidException(ErrorCodes.Student.INSUFFICIENT_CREDITS);
+    }
+
+    const enrollment = Enrollment.enroll(lecture);
+    this.#allowedCredits -= lecture.credits;
+    this.#enrollments.set(lecture.id, enrollment);
+
+    return enrollment;
+  }
+
+  cancelEnrollment(lecture: Lecture) {
+    const enrollment = this.#enrollments.get(lecture.id);
+    enrollment.cancel(lecture);
+
+    this.#allowedCredits += lecture.credits;
+  }
+
+  get allowedCredits() {
+    return this.#allowedCredits;
+  }
+}
